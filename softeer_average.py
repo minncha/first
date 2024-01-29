eb6bdbfc88fed0fd8ec7c361ab886f81609c157e
minncha <amc8574@naver.com>
2024-01-29 14:21:26 +0900

    first commit

---
+++ softeer_average.py
@@ -0,0 +1,11 @@
+import sys
+
+n,k = map(int, input().split())
+s=list(map(int, input().split()))
+
+for i in range(k):
+    a,b = map(int,input().split())
+  
+    average = sum(s[a-1:b])/(b-a+1)
+    
+    print("%0.2f"%average)
